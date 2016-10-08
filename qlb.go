206f1f247b79d8ce5d80b41a0569fc79102d67da
Dustin L. Howett <dustin@howett.net>
2016-10-08 16:49:42 -0700

    First pass: outputs csv and guesses at config params.

---
+++ qlb.go
@@ -0,0 +1,85 @@
+package main
+
+/*************************
+ * research-quality code *
+ *************************/
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+	"os"
+)
+
+type Header struct {
+	Magic   [4]byte
+	Unknown [0xE2]byte
+	NParam  uint16
+}
+
+type ParamRecord struct {
+	Type   uint16
+	Length uint16
+}
+
+type Record struct {
+	CH1 uint16
+	CH2 uint16
+}
+
+func main() {
+	f, _ := os.Open(os.Args[1])
+	defer f.Close()
+
+	var hdr Header
+	f.Seek(0x0, 0)
+	binary.Read(f, binary.LittleEndian, &hdr)
+	fmt.Printf("Magic: %4s\nNumber of config records: %d\n", hdr.Magic, hdr.NParam)
+
+	f.Seek(0x1B2, 0)
+	for i := uint16(0); i < hdr.NParam; i++ {
+		var nameLen uint16
+		binary.Read(f, binary.LittleEndian, &nameLen)
+
+		bName := make([]byte, nameLen)
+		f.Read(bName)
+		name := string(bName[:nameLen-1])
+
+		var prec ParamRecord
+		binary.Read(f, binary.LittleEndian, &prec)
+		switch prec.Type {
+		case 0x0B:
+			var val uint16
+			binary.Read(f, binary.LittleEndian, &val)
+			fmt.Printf("U16: %s = %d\n", name, val)
+		case 0x02:
+			val := make([]byte, prec.Length)
+			f.Read(val)
+			fmt.Printf("STR: %s = %s\n", name, string(val))
+		default:
+			fmt.Printf("UNK: %s (%x), bailing out\n", name, prec.Type)
+			return
+		}
+	}
+
+	outf, _ := os.Create(os.Args[1] + ".v16le.csv")
+	defer outf.Close()
+	f.Seek(0x47C+1, 0)
+	//var chs map[int]int
+	i := 0
+	fmt.Fprintf(outf, "ch1,ch2\n")
+	for {
+		var rec Record
+		err := binary.Read(f, binary.LittleEndian, &rec)
+		fmt.Fprintf(outf, "%d,%d\n", rec.CH1, rec.CH2)
+		i++
+		if (i % 10000) == 0 {
+			fmt.Fprintf(os.Stderr, "... %d ...\n", i)
+		}
+
+		if err == io.EOF {
+			break
+		}
+	}
+	fmt.Fprintf(os.Stderr, "--- %d measurements ---", i)
+}
